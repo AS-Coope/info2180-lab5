58d73c6cb26ba59b2aa5e67d0855e40dff6295cc
AS-Coope <deltazirconium@gmail.com>
2023-11-20 18:12:56 -0500

    Create variables to hold a reference to necessary HTML elements

--- world.js
+++ world.js
@@ -3,5 +3,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 function main() {
+    // create references to the necessary elements in HTML
+    let countryInput = document.getElementById("country");
+    let submitBtn = document.getElementById("lookup");
+    let resourceUrl = "http://localhost/info2180-lab5/world.php"
 
 }
