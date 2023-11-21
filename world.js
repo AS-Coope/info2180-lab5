566ffb97dedbb1ff71fd500794aa6c36be8a7dd7
AS-Coope <deltazirconium@gmail.com>
2023-11-20 19:41:45 -0500

    Create reference for result div and algorithm for fetching data

--- world.js
+++ world.js
@@ -7,5 +7,13 @@ function main() {
     let countryInput = document.getElementById("country");
     let submitBtn = document.getElementById("lookup");
     let resourceUrl = "http://localhost/info2180-lab5/world.php"
+    let result = document.getElementById("result");
 
+    // listen for button click
+    // get data from user (remember to validate)
+    // use fetch to send request to the server
+    // ensure, on server side, request is sanitized and filtered
+    // make the request to the database
+    // retrieve data (html) from server via fetch
+    // display it in result div
 }
